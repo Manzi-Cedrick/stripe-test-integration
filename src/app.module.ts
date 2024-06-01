548434deaed0ae444ec0af76db44d1198ba7b86b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-06-01 21:04:51 +0200

    [core]: project update --addition

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
+import { StripePaymentGatewayModule } from './stripe_payment_gateway/stripe_payment_gateway.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot(), StripePaymentGatewayModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
