caf6e3faa6ba91c14d4ed25dc9a59e93db9b7626
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-06-04 13:42:27 +0200

    [feat]: role-restrict access

--- src/stripe_payment_gateway/stripe_payment_gateway.controller.ts
+++ src/stripe_payment_gateway/stripe_payment_gateway.controller.ts
@@ -3,6 +3,8 @@ import { StripePaymentGatewayService } from './stripe_payment_gateway.service';
 import { CreateStripeCardTokenPaymentMethodDto, CreateStripeChargeDto, CreateStripeCustomerDto } from './dto/create-stripe_payment_gateway.dto';
 import { UpdateStripeCustomerDto } from './dto/update-stripe_payment_gateway.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('stripe-payment-gateway')
 export class StripePaymentGatewayController {
@@ -14,21 +16,29 @@ export class StripePaymentGatewayController {
     return this.stripePaymentGatewayService.createCustomer(createCustomerDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get()
   findAll() {
     return this.stripePaymentGatewayService.findAllCustomers();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.stripePaymentGatewayService.findOneCustomer(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStripePaymentGatewayDto: UpdateStripeCustomerDto) {
     return this.stripePaymentGatewayService.updateCustomer(id, updateStripePaymentGatewayDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.stripePaymentGatewayService.removeCustomer(id);
@@ -41,18 +51,21 @@ export class StripePaymentGatewayController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get('charge')
   findAllCharges() {
     return this.stripePaymentGatewayService.findAllCharges();
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get('charge/:id')
   findOneCharge(@Param('id') id: string) {
     return this.stripePaymentGatewayService.findOneCharge(id);
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Delete('charge/:id')
   removeCharge(@Param('id') id: string) {
     return this.stripePaymentGatewayService.removeCharge(id);
@@ -71,18 +84,21 @@ export class StripePaymentGatewayController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get('payment-method')
   findAllPaymentMethods() {
     return this.stripePaymentGatewayService.findAllPaymentMethods();
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Get('payment-method/:id')
   findOnePaymentMethod(@Param('id') id: string) {
     return this.stripePaymentGatewayService.findOnePaymentMethod(id);
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles(Role.ADMIN, Role.SUPERUSER)
   @Delete('payment-method/:id')
   removePaymentMethod(@Param('id') id: string) {
     return this.stripePaymentGatewayService.removePaymentMethod(id);
